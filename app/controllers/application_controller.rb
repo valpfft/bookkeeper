fffaba63472fac6465d5dee728f006577a77180b
Valiantsin Mikhaliuk <valiantsin.mikhaliuk@gmail.com>
2017-01-27 23:57:30 +0100

    * Setup devise sanitize. Now user can edit profile * Add name field in registration form * Add dropdown menu in navbar and some style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
+  end
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   enum role: [:user, :admin]
   after_initialize :set_default_role, if: :new_record?
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :validatable, :lockable
 
   def set_default_role
     self.role ||= :user
