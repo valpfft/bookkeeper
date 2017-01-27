6d22d87b3e2121532bd0411615635c9da0040588
Valiantsin Mikhaliuk <valiantsin.mikhaliuk@gmail.com>
2017-01-27 21:12:47 +0100

    * A bit off users policy setup * Now it green ;}

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   enum role: [:user, :admin]
-  after_initialize :set_default_role, if: new_record?
+  after_initialize :set_default_role, if: :new_record?
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
